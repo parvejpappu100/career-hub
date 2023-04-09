8dd71200549b63b751dd0a0f5b1c60fa8a6008ba
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2023-04-10 01:29:41 +0600

    added header banner

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Header.css'
+import Banner from '../Banner/Banner';
 
 const Header = () => {
     return (
@@ -10,7 +11,7 @@ const Header = () => {
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                       <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-sky-50 rounded-box w-52">
                       <li><a>Statistics</a></li>
                       <li><a>Applied Jobs</a></li>
                       <li><a>Blog</a></li>
@@ -28,6 +29,9 @@ const Header = () => {
                 <div className="navbar-end">
                   <a className="btn btn-job-primary text-white font-semibold">Star Applying</a>
                 </div>
+            </div>
+            <div className='lg:container mx-auto'>
+                <Banner></Banner>
             </div>
         </div>
     );
