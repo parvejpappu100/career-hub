fa538fac24bfd58b427ead9529b5090ebbff4a78
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2023-04-10 01:01:15 +0600

    added header

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,19 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Main from './components/LayOut/Main';
+import Home from './components/Home/Home';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      }
+    ]
   },
 ]);
 
